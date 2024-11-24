bdfa933ad2f60ae40a88302b2392deaef4177ff4
Annnie23 <143446906+Annnie23@users.noreply.github.com>
2024-11-24 11:27:53 +0100

    stuff

--- app/public/wp-content/themes/jacobulvchristensen/footer.php
+++ app/public/wp-content/themes/jacobulvchristensen/footer.php
@@ -1,26 +1,22 @@
-<footer class="site-footer">
+<footer class="footer">
     <div class="footer-container">
         <div class="footer-section">
             <h3>Maskinstationen Jacob Ulv Christensen</h3>
-            <div class="map">
-                <?php echo do_shortcode('[wpgmza id="1"]'); ?>
+            <div class="footer-map">
+                <!-- Google Maps Embed -->
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2262.11846288238!2d8.630329276387767!3d55.46060151446004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464b23533512a8f9%3A0x6c8132dac104879f!2sSneumg%C3%A5rdsvej%202%2C%206731%20Tj%C3%A6reborg!5e0!3m2!1sda!2sdk!4v1732438864113!5m2!1sda!2sdk" width="400" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
         <div class="footer-section">
             <h3>Kontakt os:</h3>
             <p><strong>Tlf.:</strong> <a href="tel:+4575457734">+45 75 45 77 34</a></p>
             <p><strong>Email:</strong> <a href="mailto:ju@jacobulv.dk">ju@jacobulv.dk</a></p>
-            <p><strong>Adresse:</strong><br>
-                Sneumgårdsvej 2,<br>
-                6731 Tjærringborg
-            </p>
+            <p><strong>Adresse:</strong><br> Sneumgårdsvej 2, 6731<br> Tjæreborg</p>
         </div>
         <div class="footer-section">
             <h3>Følg os:</h3>
-            <ul>
-                <li><a href="https://www.instagram.com/" target="_blank">Instagram</a></li>
-                <li><a href="https://www.facebook.com/" target="_blank">Facebook</a></li>
-            </ul>
+            <p><a href="https://www.instagram.com" target="_blank">Instagram</a></p>
+            <p><a href="https://www.facebook.com" target="_blank">Facebook</a></p>
         </div>
     </div>
 </footer>
