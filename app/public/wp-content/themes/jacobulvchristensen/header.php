2bfbeaba8538236f435c41d028d3ab158a50c414
Annnie23 <143446906+Annnie23@users.noreply.github.com>
2024-11-27 17:16:13 +0100

    nyt

--- app/public/wp-content/themes/jacobulvchristensen/header.php
+++ app/public/wp-content/themes/jacobulvchristensen/header.php
@@ -9,33 +9,35 @@
 <body <?php body_class(); ?>>
 
     <!-- Topbar -->
-<?php 
+    <?php 
 $topbar_text = get_field('topbar_text', get_the_ID()); 
 $topbar_link = get_field('topbar_link', get_the_ID()); 
 $topbar_text_2 = get_field('topbar_text_2', get_the_ID()); 
 $icon_1 = get_field('icon_1', get_the_ID()); 
 $icon_2 = get_field('icon_2', get_the_ID());
 
-if ($topbar_text): ?>
+if ($topbar_text || $topbar_text_2): ?>
     <div class="topbar">
-        <p class="topbar-text">
-            <?php if ($icon_1): ?>
-                <img src="<?php echo esc_url($icon_1); ?>" alt="Ikon" class="topbar-icon">
-            <?php endif; ?>
-            
-            <span class="topbar-text-content"><?php echo esc_html($topbar_text); ?></span>
+        <?php if ($icon_1 && $topbar_text): ?>
+            <div class="topbar-item">
+                <img src="<?php echo esc_url($icon_1); ?>" alt="Ikon 1" class="topbar-icon">
+                <span class="topbar-text-content"><?php echo esc_html($topbar_text); ?></span>
+            </div>
+        <?php endif; ?>
 
-            <?php if ($icon_2): ?>
-                <img src="<?php echo esc_url($icon_2); ?>" alt="Ikon" class="topbar-icon">
-            <?php endif; ?>
+        <?php if ($icon_2 && $topbar_text_2): ?>
+            <div class="topbar-item">
+                <img src="<?php echo esc_url($icon_2); ?>" alt="Ikon 2" class="topbar-icon">
+                <span class="topbar-text-content"><?php echo esc_html($topbar_text_2); ?></span>
+            </div>
+        <?php endif; ?>
 
-            <span class="topbar-text-content"><?php echo esc_html($topbar_text_2); ?></span>
-            <?php if ($topbar_link): ?>
-                <a href="<?php echo esc_url($topbar_link); ?>" class="topbar-link">Kontakt os idag!</a>
-            <?php endif; ?>
-        </p>
+        <?php if ($topbar_link): ?>
+            <a href="<?php echo esc_url($topbar_link); ?>" class="topbar-link">Kontakt os idag!</a>
+        <?php endif; ?>
     </div>
 <?php endif; ?>
+
 
     <!-- Slut på topbar -->
 
@@ -46,6 +48,7 @@ if ($topbar_text): ?>
                     <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>">
                 </a>
             </div>
+
             <nav class="main-navigation">
                 <?php
                 wp_nav_menu(array(
@@ -57,4 +60,9 @@ if ($topbar_text): ?>
             </nav>  
         </div>
     </header>
+
+   
+
+    
     <div id="content" class="site-content">
+   
